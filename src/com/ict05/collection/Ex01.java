6fa691bcb2ac33b2f246621a745028f2ec7d2402
user <user@DESKTOP-LHVF2H1>
2021-04-05 20:04:12 +0900

    4

--- src/com/ict05/collection/Ex01.java
+++ src/com/ict05/collection/Ex01.java
@@ -1,6 +1,8 @@
 package com.ict05.collection;
 
 public class Ex01 {
+	public static void main(String[] args) {
+		
 	// 제네닉과 컬렉션 (자료구조)
 	// 제네닉 : 컬렉션이 어떤 객체들로 이루어졌는지를 표시하는 객체 타입을 말한다.
 	// 컬렉션 : 객체들을 모아서 관리하는 인터페이스을 구현한 것
@@ -13,5 +15,7 @@ public class Ex01 {
 	
 	// Collection의 주요 메소드 
 	
-	// test  
+	// test  hjghjghjghjghjghjghj
+		
+	}
 }
